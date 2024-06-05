4a9557e2f0953b04dcc2f809dde6b1ad14a179a3
yeimy-18 <pandojulio6@gmail.com>
2024-06-05 18:46:54 -0400

    solucion de problema 1 y 2

--- ejercicio_2/problema_2.f90
+++ ejercicio_2/problema_2.f90
@@ -9,12 +9,16 @@ contains
         ! Implementación de la subrutina leedatos
     end subroutine leedatos
 
-    real function potencia(x, n) result(res)
-        implicit none
-        real, intent(in) :: x
-        integer, intent(in) :: n
-        ! Implementación de la función potencia
-    end function potencia
+    real function potencia(a, m)
+    integer, intent(in) :: m, j
+    real, intent(in) :: a
+    real :: pot = 1.0
+    do j = 1, m
+        pot = pot * a
+    end do
+    potencia = pot
+    return
+end function potencia
 end module funciones
 
 program C_potencia
@@ -42,3 +46,7 @@ program C_potencia
 
     stop 'Programa potencias termino de forma normal'
 end program C_potencia
+
+!Las diferencias al ejecutar el programa serán que ahora la función potencia tiene una firma diferente, con los argumentos a y m pasados por valor, y se declara la variable j localmente en la función. Esto puede causar un error de compilación si el programa principal no se actualiza para reflejar estos cambios en la llamada a potencia.
+
+!Para corregir esto, debes ajustar la llamada a potencia en el bucle do del programa principal para que coincida con la nueva firma de la función y asegurarte de que los argumentos se pasen correctamente.
